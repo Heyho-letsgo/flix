b7bcaedfb34a6c4be2878eb1a59021bf5d2fbae8
André Baudin-Laurencin <abaudin@gmail.com>
2014-01-31 12:58:57 +0100

    Problème passé avec le helper dans movies_helper. Le message d'erreur indiquait que je n'avais pas défini la methode. Alors que c'était la méthode qui contenait des erreurs...

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,17 @@
 module MoviesHelper
+
+  def format_total_gross(m)
+
+    if m.total_gross > 300
+      content_tag(:strong, 'Yop')
+
+    else
+      content_tag(:strong, 'Flop')
+    end
+
+  end
+
 end
+
+
+
