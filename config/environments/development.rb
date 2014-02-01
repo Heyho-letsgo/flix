7baf0e87e4f25ef830c78d972ead23536842d137
André Baudin-Laurencin <abaudin@gmail.com>
2014-02-01 18:39:46 +0100

    Jusqu'à edit... le français fonctionne un peu - les dates sont ok en saisie, mais s'affiche (show) en anglais

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,7 @@ Flix::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  #j'ai rajouté
+  I18n.enforce_available_locales = false
+
 end
